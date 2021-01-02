fa4268c6182dc709a78604a0f76e08a98561bd97
Mujahid Daud Khan <mujahid.daud@teo-intl.com>
2021-01-02 17:22:15 +0500

    Removed un necessary usings and small code refactoring

--- NodaTimeDurationBug/Program.cs
+++ NodaTimeDurationBug/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using NodaTime;
@@ -13,19 +11,21 @@ namespace NodaTimeDurationBug
 		static async Task Main()
 		{
 			await using var dbContext = new ApplicationDbContext(GetOptions());
-			// await dbContext.Database.EnsureDeletedAsync();
+			await dbContext.Database.EnsureDeletedAsync();
 			await dbContext.Database.EnsureCreatedAsync();
+
+			const int seconds = 2148;
 
 			dbContext.Add(new DurationEntity
 			{
 				Id = Guid.NewGuid(),
-				Value = Duration.FromSeconds(3600),
+				Value = Duration.FromSeconds(seconds),
 			});
 
 			dbContext.Add(new PeriodEntity
 			{
 				Id = Guid.NewGuid(),
-				Value = Period.FromSeconds(3600),
+				Value = Period.FromSeconds(seconds),
 			});
 
 			await dbContext.SaveChangesAsync();
@@ -45,15 +45,11 @@ namespace NodaTimeDurationBug
 
 		private static DbContextOptions GetOptions()
 		{
-
 			var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-			
-
 			optionsBuilder.UseNpgsql("Host=localhost;Database=TestDb;Username=postgres;Password=123456", o =>
 			{
 				o.UseNodaTime();
 			});
-			
 			return optionsBuilder.Options;
 		}
 	}
